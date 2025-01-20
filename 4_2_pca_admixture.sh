b727b9d85640796ccee11976da66a54e77d7c1ed
cocoaawil <56572871+cocoaawil@users.noreply.github.com>
2025-01-20 16:41:28 +0700

    Update 4_2_pca_admixture.sh

--- 4_2_pca_admixture.sh
+++ 4_2_pca_admixture.sh
@@ -1,3 +1,8 @@
 ipcaps cleaned_data.bed -o pca_results
 
-admixture cleaned_data.bed K
+#For loop files, you can use shell script :
+for((k=2;k<21;k++)); do admixture cleaned_data.bed k --cv=10 -j4; done
+#Where :
+# k = The number of populations (K) that was assumed for the analysis.
+# --cv = Cross-validation, 10-fold CV was used in this case. The cross-validation error is reported in the output.
+# -j = The number of thread
