182c131ca8976e46edeeec3b51aee2c63af63a68
Kris <1006124+kridsadakorn@users.noreply.github.com>
2025-01-27 23:22:42 +0100

    Update 4_2_pca_admixture.sh

--- 4_2_pca_admixture.sh
+++ 4_2_pca_admixture.sh
@@ -1,8 +1,12 @@
+# For IPCAPS
 ipcaps cleaned_data.bed -o pca_results
 
-#For loop files, you can use shell script :
-for((k=2;k<21;k++)); do admixture cleaned_data.bed k --cv=10 -j4; done
-#Where :
+# For ADMIXTURE
+# For loop files, you can use shell script :
+for((k=2;k<21;k++)); do 
+  admixture cleaned_data.bed k --cv=10 -j4; 
+done
+# Where :
 # k = The number of populations (K) that was assumed for the analysis.
 # --cv = Cross-validation, 10-fold CV was used in this case. The cross-validation error is reported in the output.
 # -j = The number of thread
